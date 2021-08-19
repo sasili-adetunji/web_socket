5cf72c0dda13bbda43d501e71056d24f8a8a8a27
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2021-08-19 12:49:01 +0100

    pass data to the server

--- client/client.js
+++ client/client.js
@@ -1,46 +1,46 @@
 (async function() {
 
-    const ws = await connectToServer();    
+  const ws = await connectToServer();
 
-    ws.onmessage = (webSocketMessage) => {
-        const messageBody = JSON.parse(webSocketMessage.data);
-        const cursor = getOrCreateCursorFor(messageBody);
-        cursor.style.transform = `translate(${messageBody.x}px, ${messageBody.y}px)`;
-    };        
-    
-    document.body.onmousemove = (evt) => {
-        const messageBody = { x: evt.clientX, y: evt.clientY };
-        ws.send(JSON.stringify(messageBody));
-    };
-        
+  $(document).on('click', 'input', function(e){
+    e.preventDefault();
+      var targetID = $(e.target).attr('id');
+      const inputField = document.getElementById('inputField')
+      if (targetID === 'submitButton') {
+        const client_data = {
+          type: "event.interaction",
+          data: `"${inputField.value}"`
+        };
+        ws.send(JSON.stringify(client_data));
+        inputField.value = "";
+      } 
+      else if (targetID === 'errorButton') {
+        const client_data = {
+          type: "event.error",
+          data: `"${uniqueId()}"`
+        };
+        ws.send(JSON.stringify(client_data));
+        inputField.value = "";
+      }
+    });
+
+    function uniqueId() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+      });
+    }
+
     async function connectToServer() {    
-        const ws = new WebSocket('ws://localhost:9898/ws');
-        return new Promise((resolve, reject) => {
-            const timer = setInterval(() => {
-                if(ws.readyState === 1) {
-                    clearInterval(timer);
-                    resolve(ws);
-                }
-            }, 10);
-        });   
+      const ws = new WebSocket('ws://localhost:9898/ws');
+      return new Promise((resolve, reject) => {
+        const timer = setInterval(() => {
+          if(ws.readyState === 1) {
+              clearInterval(timer);
+              resolve(ws);
+          }
+        }, 10);
+      });   
     }
-
-    function getOrCreateCursorFor(messageBody) {
-        const sender = messageBody.sender;
-        const existing = document.querySelector(`[data-sender='${sender}']`);
-        if (existing) {
-            return existing;
-        }
-        
-        const template = document.getElementById('cursor');
-        const cursor = template.content.firstElementChild.cloneNode(true);
-        const svgPath = cursor.getElementsByTagName('path')[0];    
-            
-        cursor.setAttribute("data-sender", sender);
-        svgPath.setAttribute('fill', `hsl(${messageBody.color}, 50%, 50%)`);    
-        document.body.appendChild(cursor);
-
-        return cursor;
-    }
-
-})();
+  })
+();
