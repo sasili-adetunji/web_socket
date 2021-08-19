5f60782250ad039b363d35bed8ebddbc26f2160a
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2021-08-19 15:00:20 +0100

    expose the txt files on http

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 9898 });
+const createServer = require('http').createServer;
 const fs = require('fs');
+var WebSocketServer = require('ws').Server;
+const parse = require('url').parse;
+
+const server = createServer((req, res) => {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+});
+
+const wss = new WebSocketServer({ noServer: true });
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
@@ -12,18 +27,28 @@ wss.on('connection', (ws) => {
     });
 });
 
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = parse(request.url);
+  if (pathname === '/ws') {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+})
+
 function processMessage(message) {
   if (message.type == "event.interaction") {
-    fs.appendFile('errors.txt', `${message.data}\n`, function (err) {
-      if (err) return console.log(err);
-    });
-  }
-  if (message.type == "event.error") {
     fs.appendFile('interactions.txt', `${message.data}\n`, function (err) {
       if (err) return console.log(err);
     });
   }
+  if (message.type == "event.error") {
+    fs.appendFile('errors.txt', `${message.data}\n`, function (err) {
+      if (err) return console.log(err);
+    });
+  }
 }
-
-
-console.log("wss up");
+const port = 9898
+server.listen(port, () => console.info(`Server is running on PORT ${port}`));
